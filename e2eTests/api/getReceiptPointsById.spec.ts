697898c10b1804d2a1fb0dcb729b111c6b7feae9
samuel-cicero <hello.samuel.cicero@gmail.com>
2025-03-13 13:19:03 -0500

    fix tests

--- e2eTests/api/getReceiptPointsById.spec.ts
+++ e2eTests/api/getReceiptPointsById.spec.ts
@@ -1,29 +1,29 @@
 import { test, expect } from '@playwright/test';
 
 const receiptMock = {
-    "retailer": "fsdfsdf",
+    "retailer": "Target",
     "purchaseDate": "2022-01-01",
-    "purchaseTime": "13:19",
+    "purchaseTime": "13:01",
     "items": [
-        {
-            "shortDescription": "test",
-            "price": "6.49"
-        }, {
-            "shortDescription": "Emils Cheese Pizza",
-            "price": "12.25"
-        }, {
-            "shortDescription": "Knorr Creamy Chicken",
-            "price": "1.26"
-        }, {
-            "shortDescription": "Doritos Nacho Cheese",
-            "price": "3.35"
-        }, {
-            "shortDescription": "   Klarbrunn 12-PK 12 FL OZ  ",
-            "price": "12.00"
-        }
+      {
+        "shortDescription": "Mountain Dew 12PK",
+        "price": "6.49"
+      },{
+        "shortDescription": "Emils Cheese Pizza",
+        "price": "12.25"
+      },{
+        "shortDescription": "Knorr Creamy Chicken",
+        "price": "1.26"
+      },{
+        "shortDescription": "Doritos Nacho Cheese",
+        "price": "3.35"
+      },{
+        "shortDescription": "   Klarbrunn 12-PK 12 FL OZ  ",
+        "price": "12.00"
+      }
     ],
-    "total": "35.99"
-};
+    "total": "35.35"
+  };
 
 const baseUrl = 'http://localhost:3000/receipts';
 
@@ -45,7 +45,7 @@ test('returns 200 status and calculated points', async ({request}) => {
 
     const {points} = await getReceiptPointsResponse.json();
     expect(getReceiptPointsResponse.status()).toBe(200);
-    expect(points).toBe(29);
+    expect(points).toBe(28);
 
 });
 
--- jest.config.js
+++ jest.config.js
@@ -2,7 +2,7 @@
 export default {
    testEnvironment: 'node',
    transform: {
-      '^.+\.tsx?$': ['ts-jest', {}],
+      '^.+.tsx?$': ['ts-jest', {}],
    },
    setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
    collectCoverage: true,
